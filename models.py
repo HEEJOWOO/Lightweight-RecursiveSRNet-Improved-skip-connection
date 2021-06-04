7beb3b3b73874d39027433b32306cb1bdbcac6cb
HEEJO WOO <61686244+HEEJOWOO@users.noreply.github.com>
2021-06-04 14:45:22 +0900

    Update models.py

--- models.py
+++ models.py
@@ -22,16 +22,6 @@ class SB_layer(nn.Module): #Small Block Layer
         layer_1x1_3x3 = self.layer_1x1_3x3(x)
         return layer_1x1_3x3
 
-class SB_layer_residual(nn.Module): #Small Block Layer
-    def __init__(self, growth_rate):
-        super(SB_layer_residual, self).__init__()
-        gr = growth_rate
-        self.layer_3x3 = nn.Conv2d(gr,gr,3,1,1)
-        
-    def forward(self, x):
-        layer_3x3 = self.layer_3x3(x)
-        return layer_3x3
-
 class Refine_layer(nn.Module): #Information Refinement Layer
     def __init__(self, growth_rate,distillation_rate):
         super(Refine_layer, self).__init__()
@@ -65,42 +55,34 @@ class RDCAB(nn.Module):
         self.remaining_channels = int(gr-(gr*dr))
         
         self.SB_layer_1 = SB_layer(gr, 0)
-        self.SB_layer_residual_1 = SB_layer_residual(gr)
         self.Refine_layer_1 = Refine_layer(gr,dr)
         self.Retain_layer_1 = Retain_layer(gr,dr)
         
         self.SB_layer_2 = SB_layer(gr, 1)
-        self.SB_layer_residual_2 = SB_layer_residual(gr)
         self.Refine_layer_2 = Refine_layer(gr,dr)
         self.Retain_layer_2 = Retain_layer(gr,dr)
         
         self.SB_layer_3 = SB_layer(gr, 2)
-        self.SB_layer_residual_3 = SB_layer_residual(gr)
         self.Refine_layer_3 = Refine_layer(gr,dr)
         self.Retain_layer_3 = Retain_layer(gr,dr)
         
         self.SB_layer_4 = SB_layer(gr, 3)
-        self.SB_layer_residual_4 = SB_layer_residual(gr)
         self.Refine_layer_4 = Refine_layer(gr,dr)
         self.Retain_layer_4 = Retain_layer(gr,dr)
         
         self.SB_layer_5 = SB_layer(gr, 4)
-        self.SB_layer_residual_5 = SB_layer_residual(gr)
         self.Refine_layer_5 = Refine_layer(gr,dr)
         self.Retain_layer_5 = Retain_layer(gr,dr)
         
         self.SB_layer_6 = SB_layer(gr, 5)
-        self.SB_layer_residual_6 = SB_layer_residual(gr)
         self.Refine_layer_6 = Refine_layer(gr,dr)
         self.Retain_layer_6 = Retain_layer(gr,dr)
         
         self.SB_layer_7 = SB_layer(gr, 6)
-        self.SB_layer_residual_7 = SB_layer_residual(gr)
         self.Refine_layer_7 = Refine_layer(gr,dr)
         self.Retain_layer_7 = Retain_layer(gr,dr)
 
         self.SB_layer_8 = SB_layer(gr, 7)
-        self.SB_layer_residual_8 = SB_layer_residual(gr)
         self.Refine_layer_8 = nn.Sequential(nn.Conv2d(self.remaining_channels,gr,3,1,1),nn.ReLU(True),nn.Conv2d(gr,gr//2,3,1,1))
 
         self.lff = nn.Conv2d((gr//2)*8, gr, kernel_size=1)
@@ -119,49 +101,49 @@ class RDCAB(nn.Module):
     def forward(self, x, sfe_residual): 
         Local_Residual = x
         
-        SB_layer_1 = self.SB_layer_1(x)+self.SB_layer_residual_1(x)
+        SB_layer_1 = self.SB_layer_1(x)+x
         distilled_c1, remaining_c1 = torch.split(SB_layer_1, (self.distilled_channels, self.remaining_channels), dim=1) # 16 48
         Refine_layer_1 = self.Refine_layer_1(remaining_c1)
         ID_concat_1 = torch.cat([distilled_c1,Refine_layer_1],1)
         LFF_layer_1 = self.Retain_layer_1(torch.cat([SB_layer_1,distilled_c1],1))
 
-        SB_layer_2 = self.SB_layer_2(torch.cat((x, ID_concat_1), 1))+self.SB_layer_residual_2(ID_concat_1)
+        SB_layer_2 = self.SB_layer_2(torch.cat((x, ID_concat_1), 1))+ID_concat_1
         distilled_c1, remaining_c1 = torch.split(SB_layer_2, (self.distilled_channels, self.remaining_channels), dim=1) # 16 48
         Refine_layer_2 = self.Refine_layer_2(remaining_c1)
         ID_concat_2 = torch.cat([distilled_c1,Refine_layer_2],1)
         LFF_layer_2 = self.Retain_layer_2(torch.cat([SB_layer_2,distilled_c1],1))
         
-        SB_layer_3 = self.SB_layer_3(torch.cat((x, ID_concat_1,ID_concat_2), 1))+self.SB_layer_residual_3(ID_concat_2)
+        SB_layer_3 = self.SB_layer_3(torch.cat((x, ID_concat_1,ID_concat_2), 1))+ID_concat_2
         distilled_c1, remaining_c1 = torch.split(SB_layer_3, (self.distilled_channels, self.remaining_channels), dim=1) # 16 48
         Refine_layer_3 = self.Refine_layer_3(remaining_c1)
         ID_concat_3 = torch.cat([distilled_c1,Refine_layer_3],1)
         LFF_layer_3 = self.Retain_layer_3(torch.cat([SB_layer_3,distilled_c1],1))
         
-        SB_layer_4 = self.SB_layer_4(torch.cat((x, ID_concat_1,ID_concat_2,ID_concat_3), 1))+self.SB_layer_residual_4(ID_concat_3)
+        SB_layer_4 = self.SB_layer_4(torch.cat((x, ID_concat_1,ID_concat_2,ID_concat_3), 1))+ID_concat_3
         distilled_c1, remaining_c1 = torch.split(SB_layer_4, (self.distilled_channels, self.remaining_channels), dim=1) # 16 48
         Refine_layer_4 = self.Refine_layer_4(remaining_c1)
         ID_concat_4 = torch.cat([distilled_c1,Refine_layer_4],1)
         LFF_layer_4 = self.Retain_layer_4(torch.cat([SB_layer_4,distilled_c1],1))
         
-        SB_layer_5 = self.SB_layer_5(torch.cat((x, ID_concat_1,ID_concat_2,ID_concat_3,ID_concat_4), 1))+self.SB_layer_residual_5(ID_concat_4)
+        SB_layer_5 = self.SB_layer_5(torch.cat((x, ID_concat_1,ID_concat_2,ID_concat_3,ID_concat_4), 1))+ID_concat_4
         distilled_c1, remaining_c1 = torch.split(SB_layer_5, (self.distilled_channels, self.remaining_channels), dim=1) # 16 48
         Refine_layer_5 = self.Refine_layer_5(remaining_c1)
         ID_concat_5 = torch.cat([distilled_c1,Refine_layer_5],1)
         LFF_layer_5 = self.Retain_layer_5(torch.cat([SB_layer_5,distilled_c1],1))
         
-        SB_layer_6 = self.SB_layer_6(torch.cat((x, ID_concat_1,ID_concat_2,ID_concat_3,ID_concat_4,ID_concat_5), 1))+self.SB_layer_residual_6(ID_concat_5)
+        SB_layer_6 = self.SB_layer_6(torch.cat((x, ID_concat_1,ID_concat_2,ID_concat_3,ID_concat_4,ID_concat_5), 1))+ID_concat_5
         distilled_c1, remaining_c1 = torch.split(SB_layer_6, (self.distilled_channels, self.remaining_channels), dim=1) # 16 48
         Refine_layer_6 = self.Refine_layer_6(remaining_c1)
         ID_concat_6 = torch.cat([distilled_c1,Refine_layer_6],1)
         LFF_layer_6 = self.Retain_layer_6(torch.cat([SB_layer_6,distilled_c1],1))
         
-        SB_layer_7 = self.SB_layer_7(torch.cat((x, ID_concat_1,ID_concat_2,ID_concat_3,ID_concat_4,ID_concat_5,ID_concat_6), 1))+self.SB_layer_residual_7(ID_concat_6)
+        SB_layer_7 = self.SB_layer_7(torch.cat((x, ID_concat_1,ID_concat_2,ID_concat_3,ID_concat_4,ID_concat_5,ID_concat_6), 1))+ID_concat_6
         distilled_c1, remaining_c1 = torch.split(SB_layer_7, (self.distilled_channels, self.remaining_channels), dim=1) # 16 48
         Refine_layer_7 = self.Refine_layer_7(remaining_c1)
         ID_concat_7 = torch.cat([distilled_c1,Refine_layer_7],1)
         LFF_layer_7 = self.Retain_layer_7(torch.cat([SB_layer_7,distilled_c1],1))
         
-        SB_layer_8 = self.SB_layer_8(torch.cat((x, ID_concat_1,ID_concat_2,ID_concat_3,ID_concat_4,ID_concat_5,ID_concat_6,ID_concat_7), 1))+self.SB_layer_residual_8(ID_concat_7)
+        SB_layer_8 = self.SB_layer_8(torch.cat((x, ID_concat_1,ID_concat_2,ID_concat_3,ID_concat_4,ID_concat_5,ID_concat_6,ID_concat_7), 1))+ID_concat_7
         distilled_c1, remaining_c1 = torch.split(SB_layer_8, (self.distilled_channels, self.remaining_channels), dim=1) # 16 48
         Refine_layer_8 = self.Refine_layer_8(remaining_c1)
         
